62dceadddf2d1038c13373f6cfd9e8b8884efe5a
zelda <zelda@ZELDA-PC>
2022-04-11 12:12:22 +0700

    Add project files.

---
+++ airline/Search Flight.Designer.vb
@@ -0,0 +1,165 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Search_Flight
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Destination = New System.Windows.Forms.ComboBox()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.Source = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label15 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(243, 573)
+        Me.DataGridView1.Margin = New System.Windows.Forms.Padding(4, 7, 4, 7)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 62
+        Me.DataGridView1.Size = New System.Drawing.Size(1353, 892)
+        Me.DataGridView1.TabIndex = 11
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.Button1.Font = New System.Drawing.Font("Palatino Linotype", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(1013, 283)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 7, 4, 7)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(177, 116)
+        Me.Button1.TabIndex = 10
+        Me.Button1.Text = "Submit"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Destination
+        '
+        Me.Destination.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest
+        Me.Destination.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems
+        Me.Destination.BackColor = System.Drawing.SystemColors.Window
+        Me.Destination.FormattingEnabled = True
+        Me.Destination.Location = New System.Drawing.Point(502, 416)
+        Me.Destination.Margin = New System.Windows.Forms.Padding(4, 7, 4, 7)
+        Me.Destination.Name = "Destination"
+        Me.Destination.Size = New System.Drawing.Size(221, 36)
+        Me.Destination.TabIndex = 9
+        '
+        'ToolTip1
+        '
+        '
+        'Source
+        '
+        Me.Source.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest
+        Me.Source.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems
+        Me.Source.BackColor = System.Drawing.SystemColors.Window
+        Me.Source.FormattingEnabled = True
+        Me.Source.Location = New System.Drawing.Point(502, 239)
+        Me.Source.Margin = New System.Windows.Forms.Padding(4, 7, 4, 7)
+        Me.Source.Name = "Source"
+        Me.Source.Size = New System.Drawing.Size(221, 36)
+        Me.Source.TabIndex = 8
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Palatino Linotype", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(237, 416)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(193, 44)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Destination"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Palatino Linotype", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(236, 239)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(120, 44)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Source"
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.Button2.Font = New System.Drawing.Font("Palatino Linotype", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(1300, 280)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(4, 7, 4, 7)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(177, 116)
+        Me.Button2.TabIndex = 12
+        Me.Button2.Text = "Back"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Font = New System.Drawing.Font("Poppins SemiBold", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label15.Location = New System.Drawing.Point(577, 81)
+        Me.Label15.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(427, 85)
+        Me.Label15.TabIndex = 70
+        Me.Label15.Text = "SEARCH FLIGHTS"
+        '
+        'Search_Flight
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 28.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(230, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(254, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(1924, 1050)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Destination)
+        Me.Controls.Add(Me.Source)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Poppins SemiBold", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4, 7, 4, 7)
+        Me.Name = "Search_Flight"
+        Me.Text = "Search_Flight"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Destination As System.Windows.Forms.ComboBox
+    Friend WithEvents ToolTip1 As System.Windows.Forms.ToolTip
+    Friend WithEvents Source As System.Windows.Forms.ComboBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label15 As System.Windows.Forms.Label
+End Class
